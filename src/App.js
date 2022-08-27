24a75d3f55ef53e23fce5c5a38410249c8427de8
Your Name <you@example.com>
2022-08-27 21:29:06 +0000

    call to async function in useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import axios from "axios";
 import AddUser from "./comp/AddUser";
 import Header from "./comp/Header";
 import Map from "./comp/Map";
 
 import { CartProvider } from "./context/CartContext";
 
+const getRequest = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 function App() {
+  const [task, setTask] = useState("");
+  useEffect(() => {
+    ///////// reuse getRequest in useEffect
+    (async () => {
+      // 
+      let response = await getRequest();
+      setTask(response.title);
+    })();
+    /////////
+  }, []);
+
   return (
     <div className="App">
       <CartProvider>
         <Header />
+        {task}
+        <br />
         <AddUser />
         <Map />
       </CartProvider>
@@ -19,4 +43,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
